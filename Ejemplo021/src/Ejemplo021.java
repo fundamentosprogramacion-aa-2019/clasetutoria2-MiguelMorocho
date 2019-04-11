4f614bc4e4736fa820cf7dd686349eed9e53b89f
MiguelMorocho <morochomiguel56@gmail.com>
2019-04-11 17:58:23 -0500

    modificacion

---
+++ Ejemplo021/src/Ejemplo021.java
@@ -0,0 +1,27 @@
+
+import static com.sun.org.apache.xalan.internal.xsltc.compiler.util.Type.Int;
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author INSPIRON
+ */
+public class Ejemplo021 {
+    public static void main(String[] args){
+        String nombre;
+        String apellido; // variables
+        int edad;
+        
+                nombre = "Miguel";
+                apellido = "Morocho";
+                edad = 20;
+                        System.out.println ("Mi nombre es:" + nombre+"\n "
+                                + "Mi apellido es:" +apellido+ "\n "
+                                + " Mi edad es:" + edad);
+    }
+}
