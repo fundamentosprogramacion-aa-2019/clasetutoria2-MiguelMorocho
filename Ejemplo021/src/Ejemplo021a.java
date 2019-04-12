987b634797fe0b786e28f36e1888be28e6dab558
MiguelMorocho <morochomiguel56@gmail.com>
2019-04-12 09:19:31 -0500

    Clase2

---
+++ Ejemplo021/src/Ejemplo021a.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author INSPIRON
+ */
+public class Ejemplo021a {
+    public static void main(String[] args){
+        String nombrePersona; // es la variable para almacenar un nombre 
+        String apellidoPersona; 
+        
+        nombrePersona = "Miguel Angel";
+        apellidoPersona = "Morocho Morocho";
+        
+        System.out.println("Mi nombre es:"+nombrePersona+"\n\n\tMi apellido es:"
+                + apellidoPersona);
+    }
+}
